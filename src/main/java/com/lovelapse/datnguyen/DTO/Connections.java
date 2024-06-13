d1c2c60c3b193ef2503e100825e25a66a0776b0b
Dat Nguyen <datnpt2004@gmail.com>
2024-06-13 11:22:08 +0700

    Add connections

--- src/main/java/com/lovelapse/datnguyen/DTO/Connections.java
+++ src/main/java/com/lovelapse/datnguyen/DTO/Connections.java
@@ -3,30 +3,28 @@ package com.lovelapse.datnguyen.DTO;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.sql.Date;
-
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Table(name="connections")
+@Table(name = "connections")
 public class Connections {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
-    @Column(name="fromuser")
-    private String from;
+    @Column(name = "fromuser")
+    private String fromUser;
 
-    @Column(name="touser")
-    private String to;
+    @Column(name = "touser")
+    private String toUser;
 
     @Column(name = "replied")
     private boolean replied;
 
     @Column(name = "daysendorreceive")
-    private String daysendorreceive;
+    private String daySendOrReceive;
 }
